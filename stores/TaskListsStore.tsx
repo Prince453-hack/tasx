97151eadc5cbfc681449e55975464ab6c2e418a7
Random <tabishnizami456@gmail.com>
2025-02-15 22:47:51 +0530

    Task List Store function and Save Callback function

---
+++ stores/TaskListsStore.tsx
@@ -0,0 +1,77 @@
+import { useUser } from "@clerk/clerk-expo";
+import * as UIReact from "tinybase/ui-react/with-schemas";
+import { createMergeableStore, NoValuesSchema } from "tinybase/with-schemas";
+import { useCreateClientPersisterAndStart } from "./persistence/useCreateClientPersisterAndStart";
+import { useCreateServerSynchronizerAndStart } from "./synchronization/useServerSynchronizationAndStart";
+import ShoppingListStore from "./TaskListStore";
+import { useCallback } from "react";
+import { randomUUID } from "expo-crypto";
+
+const TASK_ID_PREFIX = "taskListsStore-";
+
+const TABLE_SCHEMA = {
+  lists: {
+    id: { type: "string" },
+    initialContentJson: { type: "string" },
+  },
+} as const;
+
+const {
+  useCreateMergeableStore,
+  useDelRowCallback,
+  useProvideStore,
+  useRowIds,
+  useStore,
+  useTable,
+} = UIReact as UIReact.WithSchemas<[typeof TABLE_SCHEMA, NoValuesSchema]>;
+
+const useStoreId = () => TASK_ID_PREFIX + useUser().user.id;
+
+export const useAddTaskListCallback = () => {
+  const store = useStore(useStoreId());
+
+  return useCallback(
+    (name: string, description: string, emoji: string, color: string) => {
+      const id = randomUUID();
+      store.setRow("lists", id, {
+        id,
+        initialContentJson: JSON.stringify([
+          {},
+          {
+            id,
+            name,
+            description,
+            emoji,
+            color,
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString(),
+          },
+        ]),
+      });
+    },
+    [store]
+  );
+};
+
+export default function TaskListsStore() {
+  const storeId = useStoreId();
+  const store = useCreateMergeableStore(() =>
+    createMergeableStore().setTablesSchema(TABLE_SCHEMA)
+  );
+
+  useCreateClientPersisterAndStart(storeId, store);
+  useCreateServerSynchronizerAndStart(storeId, store);
+  useProvideStore(storeId, store);
+
+  const currentUserList = useTable("lists", storeId);
+
+  return Object.entries(currentUserList).map(
+    ([listId, { initialContentJson }]) => (
+      <ShoppingListStore
+        listId={listId}
+        initialContentJson={initialContentJson}
+        key={listId}
+      />
+    )
+  );
+}
